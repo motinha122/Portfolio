5b95b85b482de4e79a4ee422834e115cb2c53271
motinha122 <timota132@gmail.com>
2022-06-24 18:52:58 -0300

    buttons hover effect

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("teste")
+let $ = (w) => document.querySelector(w);
+let print = (p) => console.log(p);
+
+function hoverButtons(){
+    $("#main-text").style.marginLeft = "calc(3.5rem + 3vh)";
+}
+
+function hoverButtonsRevert(){
+    $("#main-text").style.marginLeft = "calc(3rem + 3vh)";
+}
