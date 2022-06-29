e96c4c908220f66936e3d55f1b1a2d9d398f6b82
motinha122 <timota132@gmail.com>
2022-06-29 09:16:47 -0300

    pattern change

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const $ = (w) => document.querySelector(w);
 const print = (...p) => console.log(...p);
+
+const pattern = $(".pattern");
 const text = $("main");
 
 document.addEventListener("mousemove", (e) =>{
     
     const percent = 1 - (e.clientX / window.innerWidth) - 0.5;
     const translate = percent*15;
-    print(percent,translate);
     text.style.transform = `translateX(${translate}px)`;
 })
 
